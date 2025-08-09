6a7cdb9f33ae01e9b1912c5a390c573a53179b83
Matthew Parker <matt_parker@outlook.com>
2025-08-09 11:09:37 -0400

    Update NoNinite.user.js

--- NoNinite.user.js
+++ NoNinite.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/SysAdminDoc/NoNinite
 // @version      3.3.1
 // @description  NoNinite is a userscript that transforms ninite.com. It keeps the familiar domain but replaces every function of the site adding more applications, customization tools, and using modern package managers (Winget and Chocolatey) instead of old-school Ninite installers.
-// @author       Matthew Parker, Gemini
+// @author       Matthew Parker
 // @match        https://ninite.com/
 // @icon         https://raw.githubusercontent.com/SysAdminDoc/NoNinite/refs/heads/main/assets/icons/favicon.ico
 // @downloadURL  https://github.com/SysAdminDoc/NoNinite/raw/refs/heads/main/NoNinite.user.js
